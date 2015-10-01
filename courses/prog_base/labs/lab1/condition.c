6e34e4def01e01c2ae49f3317c092649801a4c89
Nastya67 <hnastya07@gmail.com>
2015-10-01 18:22:48 +0300

    Update condition.c

--- courses/prog_base/labs/lab1/condition.c
+++ courses/prog_base/labs/lab1/condition.c
@@ -3,12 +3,13 @@
 int satisfies(int a, int b, int c) {
    int result;
     scanf("%i, %i, %i", &a, &b, &c);
-     switch(a, b, c){
-        case"a<0 && b<0 && c<0" :
+     if(){
+      if(a<0 && b<0 && c<0) {
             if(a<b && a<c){
               int modmin = abs(a);
               int sum2 = b+c;
-                if(sum2<(-256) && modmin==2, 4, 8, 16, 32, 64, 128){
+                if(sum2<(-256) && (modmin==2||modmin==4||modmin==8||modmin==16
+                                   ||modmin==32||modmin==64||modmin==128)){
                     result = 1;
                 }
                 if(abs(sum2)>modmin && sum2>(-256)){
@@ -18,7 +19,8 @@ int satisfies(int a, int b, int c) {
             if(b<a && b<c){
               int modmin = abs(b);
               int sum2 = a+c;
-                if(sum2<(-256) && modmin==2, 4, 8, 16, 32, 64, 128){
+                if(sum2<(-256) && (modmin==2||modmin==4||modmin==8||modmin==16
+                                   ||modmin==32||modmin==64||modmin==128)){
                    result = 1;
                 }
                 if(abs(sum2)>modmin && sum2>(-256)){
@@ -28,15 +30,16 @@ int satisfies(int a, int b, int c) {
             if(c<a && c<b){
               int modmin = abs(c);
               int sum2 = a+b;
-                if(sum2<(-256) && modmin==2, 4, 8, 16, 32, 64, 128){
+                if(sum2<(-256) && (modmin==2||modmin==4||modmin==8||modmin==16
+                                   ||modmin==32||modmin==64||modmin==128)){
                     result = 1;
                 }
                 if(abs(sum2)>modmin && sum2>(-256)){
                     result = 1;
                 }
             }
-         break;
-        case "a>=0 && b>=0 && c>=0":
+      }
+      if(a>=0 && b>=0 && c>=0){
             if(a>b, a>c){
                 int max = a;
                 if(b>c){
@@ -82,34 +85,40 @@ int satisfies(int a, int b, int c) {
                     }
                 }
             }
-         break;
-        default:{
-          switch(a, b, c){
-           if((a<0 && b>=0 && c>=0)||(b<0 && a>=0 && c>=0)||(c<0 && a>=0 && b>=0)){
-            case"a<0 && b>=0 && c>=0": if(a>(-256)){
+      }
+      if(a<0 && b>=0 && c>=0){
+            if(a>(-256)){
                    result =1;
-               } break;
-            case"b<0 && a>=0 && c>=0": if(b>(-256)){
+            }
+      }
+      if(b<0 && a>=0 && c>=0){
+            if(b>(-256)){
                    result = 1;
-               } break;
-            case"c<0 && a>=0 && b>=0": if(c>(-256)){
+            }
+      }
+      if(c<0 && a>=0 && b>=0){
+            if(c>(-256)){
                    result = 1;
-               } break;
-           }
-           else{
-            case"a<0 && b<0 && c>=0": if(((a+b)*5)>(-256)){
-                       result = 1;
-                   }   break;
-            case"a<0 && c<0 && b>=0": if(((a+c)*5)>(-256)){
-                       result = 1;
-                   }   break;
-            case"b<0 && c<0 && a>=0": if(((b+c)*5)>(-256)){
-                       result = 1;
-                   }   break;
-           }
-          default: result = 0;
-          }
-        }
+            }
+      }
+      if(a<0 && b<0 && c>=0){
+            if(((a+b)*5)>(-256)){
+                   result = 1;
+            }
+      }
+      if(a<0 && c<0 && b>=0){
+            if(((a+c)*5)>(-256)){
+                   result = 1;
+            }
+      }
+      if(b<0 && c<0 && a>=0){
+            if(((b+c)*5)>(-256)){
+                   result = 1;
+            }
+      }
      }
-   return result;
+     else{
+         return 0;
+     }
+     return result;
 }
