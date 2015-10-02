240c079a4006148c8c947456cf92035ae88756c6
Nastya67 <hnastya07@gmail.com>
2015-10-02 19:27:46 +0300

    Update math.c

--- courses/prog_base/labs/lab1/math.c
+++ courses/prog_base/labs/lab1/math.c
@@ -1,16 +1,14 @@
 #include <math.h>
-double calc(double x,
-            double y,
-            double z) {
+double calc(double x, double y, double z) {
    double a;
    if(x==y)
-       retern0;
+       return NAN;
    if(x==-y)
-       retern0;
+       return NAN;
    if(sin(y)==0)
-       retern NAN;
+       return NAN;
 
-a=pow(x,y+1)/pow((x-y),0.5)+y/(6*fabs(x+y))+pow(x,1/fabs(sin(y)));
+a=pow(x,(y+1))/pow((x-y),(1/z))+y/(6*fabs(x+y))+pow(x,(1/fabs(sin(y))));
 
    return a;
 }
