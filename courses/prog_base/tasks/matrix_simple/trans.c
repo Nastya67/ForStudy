424359fbea2fd9fb073cdd5ad5539f28c1af6c32
Nastya67 <hnastya07@gmail.com>
2015-11-10 22:46:14 +0200

    matrix without mistake

--- courses/prog_base/tasks/matrix_simple/trans.c
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -18,6 +18,11 @@ void rotateCCW270(int mat[4][4]){
             mat2[i][j]=mat[3-j][i];
         }
     }
+    for(i=0; i<4; i++){
+        for(j=0; j<4; j++){
+            mat[i][j]=mat2[i][j];
+        }
+    }
 }
 
 void flipH(int mat[4][4]){
@@ -27,6 +32,11 @@ void flipH(int mat[4][4]){
             mat3[i][j]=mat[i][3-j];
         }
     }
+    for(i=0; i<4; i++){
+        for(j=0; j<4; j++){
+            mat[i][j]=mat3[i][j];
+        }
+    }
 }
 
 void transposSide(int mat[4][4]){
@@ -34,6 +44,11 @@ void transposSide(int mat[4][4]){
     for(i=0; i<4; i++){
         for(j=0; j<4; j++){
             mat4[i][j]=mat[3-j][3-i];
+        }
+    }
+    for(i=0; i<4; i++){
+        for(j=0; j<4; j++){
+            mat[i][j]=mat4[i][j];
         }
     }
 }
